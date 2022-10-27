9b526997b78a08141a8b539ee3276060cedd9b2e
Md. Abdullah Al-Mamoon <almamoon18@gmail.com>
2022-10-27 12:34:55 +0600

    responsive done

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Components/Layout/Main";
 import Blogs from "../Components/Pages/Blogs/Blogs";
+import Courses from "../Components/Pages/Courses/Courses";
 import FAQ from "../Components/Pages/FAQ/FAQ";
 import Home from "../Components/Pages/Home/Home";
 
@@ -9,5 +10,6 @@ export const routes = createBrowserRouter([
         {path: '/', element: <Home></Home>},
         {path: '/blogs', element: <Blogs></Blogs>},
         {path: '/faq', element: <FAQ></FAQ>},
+        {path: '/courses', element: <Courses></Courses>},
     ]},
 ])
