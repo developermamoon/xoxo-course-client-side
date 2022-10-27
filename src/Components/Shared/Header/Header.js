595c98b8ab734c9974d38962ccff5e7015963fb7
Md. Abdullah Al-Mamoon <almamoon18@gmail.com>
2022-10-27 16:40:59 +0600

    tooltip added

--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -8,6 +8,7 @@ import { Link } from 'react-router-dom';
 import { AuthContext } from '../UserContext/UserContext';
 import './Header.css'
 import logo from '../../../Images/online-course.png'
+import ReactTooltip from 'react-tooltip';
 
 const Header = () => {
     const { user , logOut } = useContext(AuthContext);
@@ -36,10 +37,13 @@ const Header = () => {
                         <div className='navs'><Link className='m-2 m-lg-0 me-lg-3' to='/blogs'>Blogs</Link></div>
                         <div className='navs'><Link className='m-2 m-lg-0 me-lg-3' to=''>Toogle Theme</Link></div>
 
-                        <p>{user?.displayName}</p>
 
                         {user?.uid ? 
-                            <Button onClick={handleLogOut} className='m-2 m-lg-0 me-lg-3'>Logout</Button>
+                            <>
+                                <Button onClick={handleLogOut} className='m-2 m-lg-0 me-lg-3'>Logout</Button>
+
+                                <Image data-tip={user?.displayName} fluid roundedCircle style={{ height: '40px' }} src={user?.photoURL}></Image><ReactTooltip />
+                            </>
                             :
                             <>
                                 <Button className='m-2 m-lg-0 me-lg-3'><Link to='/login'>Login</Link></Button>
