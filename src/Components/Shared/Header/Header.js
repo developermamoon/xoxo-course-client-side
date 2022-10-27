3a7297710a25bb9597bf83e829efdde3ef3f0e06
Md. Abdullah Al-Mamoon <almamoon18@gmail.com>
2022-10-27 10:07:29 +0600

    first commit

---
+++ src/Components/Shared/Header/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="info" variant="dark">
+            <Container>
+                <Navbar.Brand><Link to='/'>XOXO Course</Link></Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                <Navbar.Collapse id="responsive-navbar-nav">
+
+                    <Nav className="ms-auto">
+                        <Nav.Link href="#features">Courses</Nav.Link>
+                        <Nav.Link href="#pricing">FAQ</Nav.Link>
+                        <Nav.Link href="#pricing">Blog</Nav.Link>
+                        <Nav.Link href="#pricing">Toogle Theme</Nav.Link>
+                        <Nav.Link href="#pricing">User/login</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="#deets">More deets</Nav.Link>
+                        <Nav.Link eventKey={2} href="#memes">
+                            Dank memes
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;
---
+++ src/Routes/Routes.js
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Components/Layout/Main";
+import Home from "../Components/Pages/Home/Home";
+
+export const routes = createBrowserRouter([
+    {path: '/', element: <Main></Main>, children: [
+        {path: '/', element: <Home></Home>},
+    ]},
+])
